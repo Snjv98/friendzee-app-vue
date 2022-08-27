b196eb46710175b8f6c9ca9c7a1586c54a1a9836
Sanjeev Kansari <snjvkansari@hotmail.com>
2022-08-27 17:59:44 +0530

    initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,90 @@
+<template>
+                <section id="footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div>
+                                <img class="logo" src="../../src/assets/logo/logo.png" alt="handshake_logo" />
+                                <a class="footer-brand-name" href="/">Friendzee</a>
+                            </div>
+
+                            <div class="row">
+                                <Link to=""></Link>
+                                <a class="social-icons" href="/">
+                                    <i class="fa-brands fa-google-plus-g"></i>
+                                </a>
+                                <a class="social-icons" href="/">
+                                    <i class="fa-brands fa-facebook-f"></i>
+                                </a>
+                                <a class="social-icons" href="/">
+                                    <i class="fa-brands fa-twitter"></i>
+                                </a>
+                                <a class="social-icons" href="/">
+                                    <i class="fa-brands fa-instagram"></i>
+                                </a>
+                                <a class="social-icons" href="/">
+                                    <i class="fa-brands fa-linkedin-in"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <h3>Contact us</h3>
+                            <div>
+                                <a class="contact-items" href="/"> <font-awesome-icon icon="fa-solid fa-phone" class="contact-icons"/>+91
+                                    123456987</a>
+                            </div>
+                            <hr />
+                            <div>
+                                <a href="mailto: snjvkansari@hotmail.com" :style='{ textDecoration: "none" }'>
+                                    <a class="contact-items" href="mailto: snjvkansari@hotmail.com"><font-awesome-icon icon="fa-solid fa-envelope" class="contact-icons"/>snjvkansari@hotmail.com</a>
+                                </a>
+                            </div>
+                            <hr />
+                            <div>
+                                <a class="contact-items" href='/'>
+                                <font-awesome-icon icon="fa-solid fa-location-dot" class="contact-icons"/>New rajendra
+                                    nagar, Raipur</a>
+                            </div>
+                            <hr />
+                        </div>
+
+                        <div class="col-md-3" :style="{textAlign:'left'}">
+                            <h3>About</h3>
+                            <div>
+                                <ul>
+                                    <li class="footer-nav">
+                                        <a class="footer-item" href="signup.html" target="_blank" :style='{ textDecoration: "none" }'
+                                            rel="noopener noreferrer">Home</a>
+                                    </li>
+                                    <li class="footer-nav">
+ 
+                                            <a class="footer-item" :style='{ textDecoration: "none" }' href="/" target="_blank" rel="noopener noreferrer">My
+                                                timeline</a>
+                                        
+                                    </li>
+                                    <li class="footer-nav">
+                                            <a class="footer-item" :style='{ textDecoration: "none" }' href="/" target="_blank"
+                                                rel="noopener noreferrer">My network</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div id="name">
+
+                    </div>
+
+                </div>
+                <div class="last-sticky-row">
+
+                    Web reserved @2022
+
+                </div>
+            </section>
+</template>
+<script>
+export default {
+    name:'FooTer',
+}
+</script>
---
+++ src/components/topNavbar/TopNavbar.vue
@@ -0,0 +1,66 @@
+<template>
+
+                <section id="navigation-bar">
+                <div className="">
+                    <div className="row">
+                        <nav className="navbar sticky-top navbar-dark bg-dark navbar-expand-lg p-0">
+                            <div className="container">
+                                <div>
+                                    <a href="/">
+                                        <span className="navbar-brand brand-nav" href="/">
+                                            <img className="logo" src="../../assets/logo/logo.png" alt="handshake_logo" />
+                                        </span>
+                                    </a>
+                                </div>
+                                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                    aria-expanded="false" aria-label="Toggle navigation">
+                                    <span className="navbar-toggler-icon"></span>
+                                </button>
+                                <div className="collapse navbar-collapse nav-item-container" id="navbarSupportedContent">
+                                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 col-lg-9 nav-ul">
+                                        <li className="nav-item">
+                                            <a href="/" :style="{textDecoration: 'none'}">
+                                             <!-- style={{ textDecoration: "none" }} -->
+                                                <span className="nav-link active nav-items" aria-current="page">Home</span>
+                                            </a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a href="/timeline" :style="{textDecoration: 'none'}">
+                                             <!-- style={{ textDecoration: "none" }} -->
+                                                <span className="nav-link nav-items">Timeline</span>
+                                            </a>
+                                        </li>
+                                        <li className="nav-item dropdown">
+                                            <a href="/network" :style="{textDecoration: 'none'}">
+                                             <!-- style={{ textDecoration: "none" }} -->
+                                                <span className="nav-link nav-items">
+                                                    Network
+                                                </span>
+                                            </a>
+                                        </li>
+                                        <li className="nav-item" :style="{textDecoration: 'none'}">
+                                            <a href="/" :style="{textDecoration: 'none'}">
+                                            <!--  style={{ textDecoration: "none" }} -->
+                                                <span className="nav-link nav-items disabled">About</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                    <form className="d-flex" role="search">
+                                        <input className="form-control search-input" type="search" placeholder="Search"
+                                            aria-label="Search" />
+                                         <!-- removed this className="me-2" to remove the right margin after search box  -->
+                                        <button className="btn search-btn" type="submit"><font-awesome-icon icon="fa-solid fa-magnifying-glass" /></button>
+                                    </form>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </section>
+</template>
+<script>
+export default {
+    name:'TopNavbar'
+}
+</script>
